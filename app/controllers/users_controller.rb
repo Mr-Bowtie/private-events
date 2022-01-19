537c4ac046c0c68d3087e5f3d2141ee8f59f2d2c
Ian Withers <mr.bowtie989@gmail.com>
2022-01-19 14:43:12 -0600

    fix: redirect to root from user page if no user signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(current_user.id)
+    if user_signed_in?
+      @user = User.find(current_user.id)
+    else
+      redirect_to root_path
+    end
   end
 end

a23bada5b8215d49d05355c6cc24acb8f053dafc
Ian Withers <mr.bowtie989@gmail.com>
2022-01-21 12:21:00 -0600

    feat: add scopes for past and future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,15 @@
 class Event < ApplicationRecord
+  scope :past, -> { self.where('date < ?', Date.today) }
+  scope :future, -> { self.where('date >= ?', Date.today) }
+
+  # def self.past
+  #   self.where('date < ?', Date.today)
+  # end
+
+  # def self.future
+  #   self.where('date > ?', Date.today)
+  # end
+
   belongs_to :creator, class_name: 'User'
   has_many :users_events
   has_many :attendees, through: :users_events

39d4bb1e106354b6affeceda516a4ac8c42c570b
Ian Withers <mr.bowtie989@gmail.com>
2022-01-25 12:40:21 -0600

    update: add update route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   resources :events
   resources :users_events, only: [:new, :create, :destroy]
-  resources :invitations, only: [:new, :create, :destroy]
+  resources :invitations, only: [:new, :create, :destroy, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

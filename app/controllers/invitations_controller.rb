256ce450ea71d5943916749cd2d12b8a8d71294f
Ian Withers <mr.bowtie989@gmail.com>
2022-01-25 12:39:40 -0600

    feat: attach user to event after accepting invite

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,6 @@
 class InvitationsController < ApplicationController
+  before_action :setup_invitation, only: %i[ destroy update ]
+
   def new
     @invitation = current_user.created_invitations.build
   end
@@ -9,7 +11,16 @@ class InvitationsController < ApplicationController
   end
 
   def destroy
-    Invitation.find(params[:id]).destroy
+    @invitation.destroy
+    redirect_back_or_to(Event.find(params[:event_id]))
+  end
+
+  def update
+    @invitation.update_attribute(:response, params[:response])
+    if params[:response] == 'attend'
+      @event = Event.find(params[:event_id])
+      @event.users_events.create(attendee_id: current_user.id, event_id: params[:event_id])
+    end
     redirect_back_or_to(Event.find(params[:event_id]))
   end
 
@@ -17,5 +28,10 @@ class InvitationsController < ApplicationController
 
   def invitation_params
     params.require(:invitation).permit(:invite_creator_id, :recipient_id, :event_id)
+    # params.permit(:response)
+  end
+
+  def setup_invitation
+    @invitation = Invitation.find(params[:id])
   end
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,7 @@
 class Invitation < ApplicationRecord
+  scope :responded, -> { self.where('response IS NOT NULL') }
+  scope :unresponded, -> { self.where('response IS NULL') }
+
   belongs_to :event
   belongs_to :recipient, class_name: 'User'
   belongs_to :invite_creator, class_name: 'User'
